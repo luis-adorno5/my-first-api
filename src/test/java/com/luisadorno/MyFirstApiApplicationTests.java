4d6169eb01943c1b59d5726e5cc675ccdbb7c3ca
Luis Adorno <luis.adorno5@upr.edu>
2022-07-11 14:10:37 -0400

    Updates.

---
+++ src/test/java/com/luisadorno/MyFirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisadorno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
